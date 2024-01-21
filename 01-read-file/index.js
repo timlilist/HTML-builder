bfdfdb3fbd2588a92ccfff6ecd6723b9e9db8ccb
TIM\User <timonchik2001@mail.ru>
2024-01-21 19:00:14 +0300

    fix: changed the variable type

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf8');
+let fs = require('fs');
+let path = require('path');
+let stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf8');
 stream.on('readable', function() {
     let data = stream.read();
     console.log(data);
