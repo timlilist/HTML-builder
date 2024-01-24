590d0a5016ba28cb168a2bb92c47da24f3b6e504
TIM\User <timonchik2001@mail.ru>
2024-01-24 19:22:06 +0300

    fix: checked for null

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,5 +3,7 @@ let path = require('path');
 let stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf8');
 stream.on('readable', function() {
     let data = stream.read();
-    console.log(data);
+    if(data != null) {
+        console.log(data);
+    }
 })
